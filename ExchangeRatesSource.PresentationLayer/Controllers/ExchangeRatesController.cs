46afbec53a56a1806ecf0bac4f2a1f944467c50b
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 21:49:05 +0200

    Add api versioning for exchange rates source

--- ExchangeRatesSource.PresentationLayer/Controllers/ExchangeRatesController.cs
+++ ExchangeRatesSource.PresentationLayer/Controllers/ExchangeRatesController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace ExchangeRatesSource.PresentationLayer.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
 public class ExchangeRatesController : ControllerBase
 {
     [HttpGet(Name = "GetExchangeRates")]
--- ExchangeRatesSource.PresentationLayer/Program.cs
+++ ExchangeRatesSource.PresentationLayer/Program.cs
@@ -1,15 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
