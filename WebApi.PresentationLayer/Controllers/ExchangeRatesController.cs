c4d4ddafa6cfe4b420e0682d9d146d2220260c90
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 18:29:00 +0200

    Add web api projects

--- WebApi.PresentationLayer/Controllers/ExchangeRatesController.cs
+++ WebApi.PresentationLayer/Controllers/ExchangeRatesController.cs
@@ -3,30 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApi.PresentationLayer.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ExchangeRatesController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    [HttpGet(Name = "GetAllExchangeRates")]
+    public IEnumerable<string> Get()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<ExchangeRatesController> _logger;
-
-    public ExchangeRatesController(ILogger<ExchangeRatesController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return new[] {"test"};
     }
 }
