b18509b4f5e0cc4aa9f5cee90a7750ea0ef1d40d
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 20:17:41 +0200

    Add db context

---
+++ ExchangeRatesSource.InfrastructureLayer/ExchangeRateContext.cs
@@ -0,0 +1,21 @@
+using ExchangeRatesSource.DomainLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeRatesSource.InfrastructureLayer;
+
+public class ExchangeRateContext : DbContext
+{
+    public DbSet<ExchangeRateType> Types { get; set; }
+
+    
+    public ExchangeRateContext(DbContextOptions<ExchangeRateContext> options) : base(options)
+    {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ExchangeRateType>()
+            .HasKey(row => row.Name);
+    }
+    
+}
