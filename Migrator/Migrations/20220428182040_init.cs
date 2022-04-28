44d90051cdc6f882f25d0f80a424988faff1a1a8
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 20:21:09 +0200

    Add missing package to migrator and create initial migration

---
+++ Migrator/Migrations/20220428182040_init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrator.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    LastUpdateDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Name);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
