219ac268379f9e5ed22773ac3c16e1bf10469461
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 21:50:12 +0200

    Add options of getting response from api with xml

--- ExchangeRatesSource.PresentationLayer/Program.cs
+++ ExchangeRatesSource.PresentationLayer/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Mvc.Versioning;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddXmlSerializerFormatters();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
