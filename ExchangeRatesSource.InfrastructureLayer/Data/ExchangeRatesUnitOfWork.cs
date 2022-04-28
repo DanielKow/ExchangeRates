64d2aae0bf2ce6ededd8b906cfc8b48ceb89fb59
danielkowalski <danielk.biznes@gmail.com>
2022-04-28 20:32:57 +0200

    Implementa exchange rate unit of work

---
+++ ExchangeRatesSource.InfrastructureLayer/Data/ExchangeRatesUnitOfWork.cs
@@ -0,0 +1,23 @@
+using ExchangeRatesSource.ApplicationLayer;
+using ExchangeRatesSource.DomainLayer;
+
+namespace ExchangeRatesSource.InfrastructureLayer.Data;
+
+public class ExchangeRatesUnitOfWork : IExchangeRatesUnitOfWork
+{
+    public IGenericRepository<ExchangeRateType> ExchangeRateType { get; }
+    private readonly ExchangeRateContext _context;
+
+    public ExchangeRatesUnitOfWork(
+        ExchangeRateContext context,
+        IGenericRepository<ExchangeRateType> exchangeRateTypeRepository)
+    {
+        _context = context;
+        ExchangeRateType = exchangeRateTypeRepository;
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
