9931e538aa25a087a559a44078bf97db8252f8e6
danielkowalski <danielk.biznes@gmail.com>
2022-05-01 12:26:51 +0200

    Add nbp url

--- WebApi.PresentationLayer/Program.cs
+++ WebApi.PresentationLayer/Program.cs
@@ -1,22 +1,27 @@
 using ExchangeRatesSource.ApplicationLayer;
-using ExchangeRatesSource.ApplicationLayer.Services;
-using ExchangeRatesSource.InfrastructureLayer.Services;
 using ExchangeRatesSource.Nbp.InfrastructureLayer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using WebApi.ApplicationLayer;
 using WebApi.ApplicationLayer.GetExchangeRatesChain;
 using WebApi.DomainLayer.Config;
+using WebApi.InfrastructureLayer;
 using WebApi.InfrastructureLayer.GetExchangeRatesChain;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 builder.Services.AddScoped<IExternalSourcesConfig, ExternalSourcesConfig>();
 builder.Services.AddScoped<IInternalSourcesConfig, InternalSourcesConfig>();
 builder.Services.AddScoped<IExchangeRatesSource, NbpExchangeRatesSource>();
 builder.Services.AddScoped<IGetExchangeRatesChainBuilder, GetExchangeRatesChainBuilder>();
-builder.Services.AddScoped<IGetExchangeRatesService, GetExchangeRatesService>();
+builder.Services.AddScoped<IExchangeRatesService, ExchangeRatesService>();
 
 builder.Services.AddHttpClient<GetExchangeRatesChainBuilder>();
+builder.Services.AddHttpClient<NbpExchangeRatesSource>(client =>
+{
+    client.BaseAddress = new Uri(configuration["NbpUrl"]);
+});
 
 builder.Services.AddControllers()
     .AddXmlSerializerFormatters();
